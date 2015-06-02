8c6beca38ec5bb5512809cc042dbb972f5466356
mbohun <martin.bohun@gmail.com>
2015-06-02 18:35:51 +1000

    only a note, about a failed test/attempt to hook the ALA user profile check/creation into an attribute filter

--- src/main/java/org/jasig/services/support/ALARegisteredServiceAttributeFilter.java
+++ src/main/java/org/jasig/services/support/ALARegisteredServiceAttributeFilter.java
@@ -3,6 +3,7 @@ package org.jasig.cas.services.support;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Iterator;
 
 import org.jasig.cas.services.RegisteredService;
 import org.jasig.cas.services.RegisteredServiceAttributeFilter;
@@ -10,12 +11,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- * The default filter that is responsible to make sure only the allowed attributes for a given
- * registered service are released. The allowed attributes are cross checked against
- * the list of principal attributes and those that are a match will be released.
- *
- * If the registered service is set to ignore the attribute release policy, the filter
- * will release all principal attributes.
+ * This is to test if a custom attribute filter can be used to implement:
+ * - ALA profile creation/setup (from the received Facebook/Google/etc. attributes)
+ * - return custom ALA attributes back to CAS client-s
  *
  * @author Martin Bohun
  * @since 4.0.0
@@ -41,19 +39,36 @@ public final class ALARegisteredServiceAttributeFilter implements RegisteredServ
 	    logger.debug("Service [{}] is setting ALA user profile attributes.",
 			 registeredService.getName());
 
-	    /* TODO: here we:
+	    final StringBuffer buffy = new StringBuffer("\n");
+	    final Iterator keys = givenAttributes.keySet().iterator();
+	    while (keys.hasNext()) {
+		final String k = (String)keys.next();
+		buffy.append("givenAttributes[" + k + "]:" + givenAttributes.get(k) + "\n");
+	    }
+
+	    logger.debug(buffy.toString());
+
+	    /* UPDATE: this does NOT work, regardless on what you put into the result/return map, it won't allow you to:
+	       - add a new attribute
+	       - change a value of an existing attribute
+	       we could still use this spot to check (and create if not present) ALA user profile in the userdetails sql DB, but most likely that too
+	       has to be done, or better done somewhere else.
+
+	      TODO: here we:
 	       1. take the givenAttributes Map (received from facebook/google/twitter/github/etc.)
 	       2. take the email address and check if the user exist in the userdetails DB ()
 	          2.1 if the user does NOT exist we use the givenAttributes Map "automatically" register/create
 		      an ALA user in the userdetails DB and activate her/him (like if the registered manually and confirmed the registration email)
 	       3. from here on we continue using the existing emmet.sp_get_user_attributes(@p_username); where @p_username is the user email
 	          we received from facebook/google/twitter/github/etc.
-	    */
+
 	    attributes.put("email",     "martin.bohun@gmail.com");
 	    attributes.put("firstname", "Martin");
 	    attributes.put("lastname",  "Bohun");
 	    attributes.put("userid",    "2");
 	    attributes.put("authority", "ROLE_USER");
+
+	    */
         }
 	
         return Collections.unmodifiableMap(attributes);
